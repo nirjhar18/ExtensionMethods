508b2b4a5a130b1d774e489e18077f1a2e80a6fd
Nirjhar Vermani <nirjhar18@gmail.com>
2019-07-10 15:43:12 -0500

    Add files via upload

---
+++ IntExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    //we have created an IntExtensions class under the ExtensionMethods namespace in the following example.
+    //The IntExtensions class will contain all the extension methods applicable to int data type. (You may use any name for namespace and class.)
+    public static class IntExtensions
+    {
+        // define a static method as an extension method where the first parameter of the extension method specifies the type 
+        //on which the extension method is applicable.We are going to use this extension method on int type. So the first parameter must
+        //be int preceded with the this modifier.
+        public static bool IsGreaterThan(this int i, int value)
+        {
+            return i > value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    //Extension methods, as the name suggests, are additional methods. Extension methods allow you to inject additional methods without modifying,
+    //deriving or recompiling the original class, struct or interface. Extension methods can be added to your own custom class, .NET framework classes,
+    //or third party classes or interfaces.
+
+    class Program
+    {
+        //Now, you can include the ExtensionMethods namespace wherever you want to use this extension method.
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            bool result = i.IsGreaterThan(100);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
